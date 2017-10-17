62d67b327f2d8ad13d83d9856cf7ea2f1313410b
Paul Lesur <git@lesurpaul.fr>
2017-10-17 22:48:54 +0200

    add apply_options

--- src/lib.rs
+++ src/lib.rs
@@ -19,18 +19,31 @@ fn create_optional_struct(ast: &syn::DeriveInput) -> quote::Tokens {
     if let syn::Body::Struct(ref variant_data) = ast.body {
         if let &syn::VariantData::Struct(ref fields) = variant_data {
 
-            let mut attributes = quote! {};
+            let mut attributes = quote!{};
+            let mut assigners = quote!{};
             for field in fields {
                 let ref type_name = &field.ty;
                 let ref field_name = &field.ident.clone().unwrap();
                 let next_attribute = quote! { pub Option<#type_name> #field_name, };
                 attributes = quote!{ #attributes #next_attribute };
-            }
+                let next_assigner = quote!{
+                    if let Some(attribute) = optional_struct.#field_name {
+                        self.#field_name = attribute;
+                    }
+                };
+                assigners = quote!{ #assigners #next_assigner };
 
-            return quote!{
-                struct Optional#name {
-                    #attributes
-                }
+                return quote!{
+                    struct Optional#name {
+                        #attributes
+                    }
+
+                    impl #name {
+                        pub fn apply_options(&mut self, optional_struct: &Optional#name) {
+                            #assigners 
+                        }
+                    }
+                };
             }
         }
     }
@@ -38,8 +51,10 @@ fn create_optional_struct(ast: &syn::DeriveInput) -> quote::Tokens {
     panic!("OptionalStruct only supports non-tuple structs for now");
 }
 
+
 #[cfg(test)]
 mod tests {
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
