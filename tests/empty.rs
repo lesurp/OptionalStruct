5208806e6ad086ea835dab8c098193c98d8523d1
Paul Lesur <git@lesurpaul.fr>
2017-10-19 16:03:10 +0200

    add empty method and associated test

---
+++ tests/empty.rs
@@ -0,0 +1,18 @@
+#[macro_use]
+extern crate optional_struct;
+
+#[derive(OptionalStruct)]
+struct Config {
+    delay: Option<u32>,
+    path: String,
+    percentage: f32,
+}
+
+#[test]
+fn test_apply_options() {
+    let opt_config = OptionalConfig::empty();
+
+    assert_eq!(opt_config.delay, None);
+    assert_eq!(opt_config.path, None);
+    assert_eq!(opt_config.percentage, None);
+}
