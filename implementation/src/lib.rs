fc23a2ab13d07e5da819811be050baae72a26842
Paul Lesur <git@lesurpaul.fr>
2023-12-17 19:11:31 +0100

    start implementing builder pattern

--- implementation/src/lib.rs
+++ implementation/src/lib.rs
@@ -4,18 +4,148 @@ use proc_macro2::{TokenStream, TokenTree};
 use quote::{format_ident, quote, ToTokens};
 use syn::{Attribute, Data, DeriveInput, Field, Fields, Ident, Path, spanned::Spanned, Token, Type, Visibility};
 use syn::parse::{Parse, ParseStream};
+use syn::punctuated::Punctuated;
+use syn::token::Comma;
+
+const RENAME_ATTRIBUTE: &str = "optional_rename";
+const SKIP_WRAP_ATTRIBUTE: &str = "optional_skip_wrap";
+const WRAP_ATTRIBUTE: &str = "optional_wrap";
 
 #[cfg(test)]
 mod test;
 
-struct StructAttribute {
+struct DeriveInputWrapper {
+    orig: DeriveInput,
+    new: DeriveInput,
+}
+
+enum WhichStruct {
+    Original,
+    New,
+}
+
+impl DeriveInputWrapper {
+    fn new(derive_input: DeriveInput) -> Self {
+        DeriveInputWrapper { orig: derive_input.clone(), new: derive_input }
+    }
+
+    fn original(&self) -> &DeriveInput {
+        &self.orig
+    }
+
+    fn new_struct(&self) -> &DeriveInput {
+        &self.new
+    }
+
+    fn set_new_struct_fields(&mut self, macro_params: &MacroParameters) {
+        self.iter_struct_fields(WhichStruct::New, macro_params.into())
+    }
+
+    fn set_new_name(&mut self, new_name: &str) {
+        self.new.ident = Ident::new(new_name, self.new.ident.span());
+    }
+
+    fn get_derive_macros(
+        &self,
+        extra_derive: &[String],
+    ) -> TokenStream {
+        let mut extra_derive = extra_derive.iter().collect::<HashSet<_>>();
+        for attributes in &self.new.attrs {
+            let _ = attributes.parse_nested_meta(|derived_trait|
+                {
+                    let derived_trait = derived_trait.path;
+                    let full_path = quote! { #derived_trait };
+                    extra_derive.remove(&full_path.to_string());
+                    Ok(())
+                });
+        }
+
+
+        let mut acc = quote! {};
+        for left_trait_to_derive in extra_derive {
+            let left_trait_to_derive = format_ident!("{left_trait_to_derive}");
+            acc = quote! { # left_trait_to_derive, # acc};
+        }
+
+        quote! { #[derive(#acc)] }
+    }
+
+    fn fields(&self, which_struct: WhichStruct) -> &Punctuated<Field, Comma> {
+        let s = match which_struct {
+            WhichStruct::Original => &self.orig,
+            WhichStruct::New => &self.new,
+        };
+
+        let data_struct = match &s.data {
+            Data::Struct(data_struct) => data_struct,
+            _ => panic!("OptionalStruct only works for structs :)"),
+        };
+
+        match &data_struct.fields {
+            Fields::Unnamed(f) => &f.unnamed,
+            Fields::Named(f) => &f.named,
+            Fields::Unit => unreachable!("A struct cannot have simply a unit field?"),
+        }
+    }
+
+    fn fields_mut(&mut self, which_struct: WhichStruct) -> &mut Punctuated<Field, Comma> {
+        let s = match which_struct {
+            WhichStruct::Original => &mut self.orig,
+            WhichStruct::New => &mut self.new,
+        };
+
+        let data_struct = match &mut s.data {
+            Data::Struct(data_struct) => data_struct,
+            _ => panic!("OptionalStruct only works for structs :)"),
+        };
+
+        match &mut data_struct.fields {
+            Fields::Unnamed(f) => &mut f.unnamed,
+            Fields::Named(f) => &mut f.named,
+            Fields::Unit => unreachable!("A struct cannot have simply a unit field?"),
+        }
+    }
+
+
+    fn iter_struct_fields(&mut self, which_struct: WhichStruct, new_struct_params: Option<&MacroParameters>) {
+        // TODO: has to be a cleaner way
+        let (apply_attribute_metadata, default_wrapping, make_fields_public) = if let Some(n) = new_struct_params {
+            (true, n.default_wrapping_behavior, n.make_fields_public)
+        } else {
+            (false, false, false)
+        };
+
+        for field in self.fields_mut(which_struct).iter_mut() {
+            let field_meta_data = extract_relevant_attributes(field, default_wrapping);
+            if apply_attribute_metadata {
+                field_meta_data.apply_to_field(field);
+                if make_fields_public {
+                    field.vis = Visibility::Public(syn::token::Pub(field.vis.span()))
+                }
+            }
+        }
+    }
+
+    fn finalize_definition(mut self, macro_parameters: &MacroParameters) -> (TokenStream, TokenStream) {
+        // https://github.com/rust-lang/rust/issues/65823 :(
+        self.iter_struct_fields(WhichStruct::Original, None);
+
+        let derives = self.get_derive_macros(&macro_parameters.extra_derive);
+
+        let orig = self.orig;
+        let new = self.new;
+        (quote! { #orig }, quote! { #derives #new })
+    }
+}
+
+struct ParsedMacroParameters {
     new_struct_name: Option<String>,
     default_wrapping: bool,
 }
 
-impl Parse for StructAttribute {
+impl Parse for ParsedMacroParameters {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        let mut out = StructAttribute {
+        let mut out = ParsedMacroParameters {
             new_struct_name: None,
             default_wrapping: true,
         };
@@ -88,80 +218,29 @@ fn is_type_option(t: &Type) -> bool {
     }
 }
 
-struct GlobalAttributes {
-    new_struct_name: Option<String>,
+struct MacroParameters {
+    new_struct_name: String,
     extra_derive: Vec<String>,
-    field_attributes: GlobalFieldAttributes,
-}
-
-struct GlobalFieldAttributes {
     default_wrapping_behavior: bool,
     make_fields_public: bool,
 }
 
-impl GlobalAttributes {
-    // TODO: should use named arguments
-    fn new(attr: StructAttribute) -> Self {
-        let new_struct_name = attr.new_struct_name;
+impl MacroParameters {
+    fn new(attr: ParsedMacroParameters, struct_definition: &DeriveInput) -> Self {
+        let new_struct_name = attr.new_struct_name.unwrap_or_else(|| "Optional".to_owned() + &struct_definition.ident.to_string());
         let default_wrapping_behavior = attr.default_wrapping;
-        GlobalAttributes {
+        MacroParameters {
             new_struct_name,
             extra_derive: vec!["Clone", "PartialEq", "Default", "Debug"]
                 .into_iter()
                 .map(|s| s.to_owned())
                 .collect(),
-            field_attributes: GlobalFieldAttributes {
-                default_wrapping_behavior,
-                // TODO;
-                make_fields_public: true,
-            },
+            default_wrapping_behavior,
+            make_fields_public: true,
         }
     }
 }
 
-fn set_new_struct_name(new_name: Option<String>, new_struct: &mut DeriveInput) {
-    let new_struct_name =
-        new_name.unwrap_or_else(|| "Optional".to_owned() + &new_struct.ident.to_string());
-
-    new_struct.ident = Ident::new(&new_struct_name, new_struct.ident.span());
-}
-
-fn iter_struct_fields(the_struct: &mut DeriveInput, global_att: Option<&GlobalFieldAttributes>) {
-    // TODO: has to be a cleaner way
-    let (apply_attribute_metadata, default_wrapping, make_fields_public) = match global_att {
-        Some(ga) => (true, ga.default_wrapping_behavior, ga.make_fields_public),
-        None => (false, false, false),
-    };
-    let data_struct = match &mut the_struct.data {
-        Data::Struct(data_struct) => data_struct,
-        _ => panic!("OptionalStruct only works for structs :)"),
-    };
-
-    let fields = match &mut data_struct.fields {
-        Fields::Unnamed(f) => &mut f.unnamed,
-        Fields::Named(f) => &mut f.named,
-        Fields::Unit => unreachable!("A struct cannot have simply a unit field?"),
-    };
-
-    for field in fields.iter_mut() {
-        let field_meta_data = extract_relevant_attributes(field, default_wrapping);
-        if apply_attribute_metadata {
-            field_meta_data.apply_to_field(field);
-            if make_fields_public {
-                field.vis = Visibility::Public(syn::token::Pub(field.vis.span()))
-            }
-        }
-    }
-}
-
-fn set_new_struct_fields(new_struct: &mut DeriveInput, global_att: &GlobalFieldAttributes) {
-    iter_struct_fields(new_struct, global_att.into())
-}
-
-fn remove_optional_struct_attributes(original_struct: &mut DeriveInput) {
-    // Last boolean isn't actually used but w/e
-    iter_struct_fields(original_struct, None)
-}
 
 struct FieldAttributeData {
     wrap: bool,
@@ -185,10 +264,6 @@ impl FieldAttributeData {
 }
 
 fn extract_relevant_attributes(field: &mut Field, default_wrapping: bool) -> FieldAttributeData {
-    const RENAME_ATTRIBUTE: &str = "optional_rename";
-    const SKIP_WRAP_ATTRIBUTE: &str = "optional_skip_wrap";
-    const WRAP_ATTRIBUTE: &str = "optional_wrap";
-
     let mut field_attribute_data = FieldAttributeData {
         wrap: !is_type_option(&field.ty) && default_wrapping,
         new_type: None,
@@ -240,19 +315,35 @@ fn acc_assigning<'a, T: Iterator<Item=(U, &'a Vec<Attribute>)>, U: std::borrow::
     acc
 }
 
-fn generate_apply_fn(
-    derive_input: &DeriveInput,
-    new_struct: &DeriveInput,
+fn acc_check<'a, T: Iterator<Item=(U, &'a Vec<Attribute>)>, U: std::borrow::Borrow<V>, V: ToTokens>(
+    idents_with_attrs: T,
 ) -> TokenStream {
-    let orig_name = &derive_input.ident;
-    let new_name = &new_struct.ident;
+    let mut acc = quote! {};
+    for (ident, attrs) in idents_with_attrs {
+        let ident = ident.borrow();
+        let cfg_attr = attrs.iter().find(|attr| attr.path().is_ident("cfg"));
+        acc = quote! {
+            #acc
 
-    let fields = match &derive_input.data {
+            #cfg_attr
+            if !self.#ident.can_be_applied() { return false; }
+        };
+    }
+    acc
+}
+
+fn generate_apply_fn(
+    derive_input: &DeriveInputWrapper,
+) -> TokenStream {
+    let orig_name = &derive_input.original().ident;
+    let new_name = &derive_input.new_struct().ident;
+
+    let fields = match &derive_input.original().data {
         Data::Struct(s) => &s.fields,
         _ => unreachable!(),
     };
 
-    let acc = match &fields {
+    let apply_to_acc = match &fields {
         Fields::Unit => unreachable!(),
         Fields::Named(fields_named) => {
             let it = fields_named.named.iter().map(|f| (f.ident.as_ref().unwrap(), &f.attrs));
@@ -267,8 +358,25 @@ fn generate_apply_fn(
         }
     };
 
-    let (impl_generics, ty_generics, where_clause) = derive_input.generics.split_for_impl();
+    // TODO: copy pasted code
+    let can_apply_acc = match &fields {
+        Fields::Unit => unreachable!(),
+        Fields::Named(fields_named) => {
+            let it = fields_named.named.iter().map(|f| (f.ident.as_ref().unwrap(), &f.attrs));
+            acc_check::<_, _, Ident>(it)
+        }
+        Fields::Unnamed(fields_unnamed) => {
+            let it = fields_unnamed.unnamed.iter().enumerate().map(|(i, field)| {
+                let i = syn::Index::from(i);
+                (quote! {#i}, &field.attrs)
+            });
+            acc_check(it)
+        }
+    };
+
+    let (impl_generics, ty_generics, where_clause) = derive_input.original().generics.split_for_impl();
     quote! {
+        /*
         impl #impl_generics Applyable<#orig_name #ty_generics> #where_clause for Option<#new_name #ty_generics >{
             fn apply_to(self, t: &mut #orig_name #ty_generics) {
                 if let Some(s) = self {
@@ -276,39 +384,92 @@ fn generate_apply_fn(
                 }
             }
         }
+        */
 
         impl #impl_generics Applyable<#orig_name #ty_generics> #where_clause for #new_name #ty_generics {
             fn apply_to(self, t: &mut #orig_name #ty_generics) {
-                #acc
+                #apply_to_acc
+            }
+
+            fn can_be_applied(&self) -> bool {
+                #can_apply_acc
+                true
             }
         }
     }
 }
 
-fn get_derive_macros(
-    new_struct: &mut DeriveInput,
-    extra_derive: &[String],
-) -> TokenStream {
-    let mut extra_derive = extra_derive.iter().collect::<HashSet<_>>();
-    for attributes in &mut new_struct.attrs {
-        let _ = attributes.parse_nested_meta(|derived_trait|
-            {
-                let derived_trait = derived_trait.path;
-                let full_path = quote! { #derived_trait };
-                extra_derive.remove(&full_path.to_string());
-                Ok(())
-            });
+fn generate_try_from_impl(derive_input: &DeriveInputWrapper, macro_parameters: &MacroParameters) -> TokenStream {
+    let old_name = &derive_input.original().ident;
+    let new_name = &derive_input.new_struct().ident;
+
+    let (impl_generics, ty_generics, where_clause) = derive_input.new_struct().generics.split_for_impl();
+
+    let mut field_check_acc = quote! { };
+    let mut field_assign_acc = quote! { };
+    for (i, field) in derive_input.fields(WhichStruct::Original).iter().enumerate() {
+        let is_wrapped = field.attrs
+            .iter()
+            .rev().find_map(
+            |a| {
+                if a.path().is_ident(SKIP_WRAP_ATTRIBUTE) {
+                    Some(false)
+                } else if a.path().is_ident(WRAP_ATTRIBUTE) {
+                    Some(true)
+                } else {
+                    None
+                }
+            }).unwrap_or(!is_type_option(&field.ty) && macro_parameters.default_wrapping_behavior);
+
+        let ident = field.ident.as_ref().map(|id| quote! { #id }).unwrap_or_else(|| {
+            let index = syn::Index::from(i);
+            quote! { #index }
+        });
+
+        // TODO: we're looping twice on this
+        let cfg_attr = field.attrs.iter().find(|attr| attr.path().is_ident("cfg"));
+
+        let (unwrap, check) = if is_wrapped {
+            (
+                quote!{ .unwrap() },
+                quote!{ #cfg_attr if v.#ident.is_none() { return Err(v); } }
+            )
+        } else {
+            (
+            quote!{},
+            quote!{}
+            )
+        };
+
+
+        field_assign_acc = quote! {
+            #field_assign_acc
+
+            #cfg_attr
+            #ident: v.#ident #unwrap,
+        };
+
+        field_check_acc = quote!{
+            #field_check_acc
+
+            #check
+        };
     }
 
+    quote! {
+        impl #impl_generics TryFrom<#new_name #ty_generics > #where_clause for #old_name #ty_generics {
+            type Error = #new_name #ty_generics;
 
-    let mut acc = quote! {};
-    for left_trait_to_derive in extra_derive {
-        let left_trait_to_derive = format_ident!("{left_trait_to_derive}");
-        acc = quote! { # left_trait_to_derive, # acc};
+            fn try_from(v: Self::Error) -> Result<Self, Self::Error> {
+                #field_check_acc
+                Ok(Self {
+                    #field_assign_acc
+                })
+            }
+        }
     }
-
-    quote! { #[derive(#acc)] }
 }
+
 
 pub struct OptionalStructOutput {
     pub original: TokenStream,
@@ -319,25 +480,24 @@ pub fn opt_struct(
     attr: TokenStream,
     input: TokenStream,
 ) -> OptionalStructOutput {
-    let attr = syn::parse2::<_>(attr).unwrap();
-    let global_att = GlobalAttributes::new(attr);
-    let mut derive_input = syn::parse2::<DeriveInput>(input).unwrap();
-    let mut new_struct = derive_input.clone();
+    let mut derive_input = DeriveInputWrapper::new(syn::parse2::<DeriveInput>(input).unwrap());
+    let macro_params = MacroParameters::new(syn::parse2::<_>(attr).unwrap(), derive_input.original());
 
-    set_new_struct_name(global_att.new_struct_name, &mut new_struct);
-    set_new_struct_fields(&mut new_struct, &global_att.field_attributes);
-    let derives = get_derive_macros(&mut new_struct, &global_att.extra_derive);
-    // https://github.com/rust-lang/rust/issues/65823 :(
-    remove_optional_struct_attributes(&mut derive_input);
-    let apply_fn_impl = generate_apply_fn(&derive_input, &new_struct);
+    derive_input.set_new_name(&macro_params.new_struct_name);
+    derive_input.set_new_struct_fields(&macro_params);
+    let apply_fn_impl = generate_apply_fn(&derive_input);
+    let try_from_impl = generate_try_from_impl(&derive_input, &macro_params);
+
+    let (original, new) = derive_input.finalize_definition(&macro_params);
 
     OptionalStructOutput {
-        original: quote! { #derive_input },
+        original,
         generated: quote! {
-            #derives
-            #new_struct
+            #new
 
             #apply_fn_impl
+
+            #try_from_impl
         },
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,23 @@
+#![feature(specialization)]
+
 pub use optional_struct_export::optional_struct;
 
 pub trait Applyable<T> {
     fn apply_to(self, t: &mut T);
+
+    fn can_be_applied(&self) -> bool {
+        true
+    }
+}
+
+impl<T> Applyable<T> for T {
+    fn apply_to(self, t: &mut T) {
+        *t = self;
+    }
+
+    fn can_be_applied(&self) -> bool {
+        true
+    }
 }
 
 impl<T> Applyable<T> for Option<T> {
@@ -10,10 +26,30 @@ impl<T> Applyable<T> for Option<T> {
             *t = s;
         }
     }
+
+    fn can_be_applied(&self) -> bool {
+        self.is_some()
+    }
 }
 
-impl<T> Applyable<T> for T {
-    fn apply_to(self, t: &mut T) {
-        *t = self;
+pub trait OptionalStructWrapperInternal: Sized + TryInto<<Self as OptionalStructWrapperInternal>::Raw, Error=Self> + Applyable<<Self as OptionalStructWrapperInternal>::Raw> {
+    type Raw;
+}
+
+pub struct OptionalBuilder<T> {
+    t: T,
+}
+
+impl<T: OptionalStructWrapperInternal<Raw = T>> OptionalBuilder<T> {
+    fn new(t: T) -> Self {
+        OptionalBuilder { t }
+    }
+    fn apply(mut self, t: T) -> Self {
+        t.apply_to(&mut self.t);
+        self
+    }
+
+    fn build(self) -> Result<T::Raw, T> {
+        self.t.try_into()
     }
 }
