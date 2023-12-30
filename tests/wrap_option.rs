749cbe902560bb57f1038d782ddfb0417de1749e
Paul Lesur <git@lesurpaul.fr>
2023-12-30 18:24:37 +0100

    remove duplicate test

--- tests/wrap_option.rs
+++ tests/wrap_option.rs
@@ -1,20 +1,6 @@
 use optional_struct::*;
 
 #[optional_struct]
-struct Foo {
-    bar: Option<u8>,
-    #[optional_wrap]
-    baz: Option<u8>
-}
-
-#[test]
-fn test_force_wrap_option() {
-    let _opt_foo = OptionalFoo {
-        bar: Some(1),
-        baz: Some(Some(2)),
-    };
-}
-#[optional_struct]
 struct Config {
     #[optional_wrap]
     delay: Option<u32>,
